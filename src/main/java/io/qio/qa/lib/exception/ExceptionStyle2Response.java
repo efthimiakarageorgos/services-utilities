51c8b52d9bcc2dc6a9ec92529c81bf40f07b2e03
Effie Karageorgos <effie.karageorgos@qiotec.com>
2016-10-28 16:14:23 -0400

    adding a new type of response

---
+++ src/main/java/io/qio/qa/lib/exception/ExceptionStyle2Response.java
@@ -0,0 +1,13 @@
+/**
+ * © Qio Technologies Ltd. 2016. All rights reserved.
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION OF QIO TECHNOLOGIES LTD.
+ */
+package io.qio.qa.lib.exception;
+
+public class ExceptionStyle2Response {
+	private String errors;
+
+	public String getErrors() {
+		return errors;
+	}
+}
