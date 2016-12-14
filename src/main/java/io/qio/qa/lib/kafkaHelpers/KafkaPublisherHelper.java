e5f490ef8b9f76eed8a42420dbda3ec6195e4379
effie.karageorgos@qiotec.com <effie.karageorgos@qiotec.com>
2016-12-13 23:05:07 -0500

    additions to  Kafka Publisher

--- src/main/java/io/qio/qa/lib/kafkaHelpers/KafkaPublisherHelper.java
+++ src/main/java/io/qio/qa/lib/kafkaHelpers/KafkaPublisherHelper.java
@@ -6,6 +6,7 @@ package io.qio.qa.lib.kafkaHelpers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 
@@ -15,6 +16,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import org.json.simple.JSONArray;
 
 public class KafkaPublisherHelper {
     public void publishMessagesContainedInFile(String[] args) throws IOException, ExecutionException, InterruptedException {
@@ -65,6 +67,86 @@ public class KafkaPublisherHelper {
                 JsonNode object = objects.next();
                 if (!object.isObject()) {
                     System.out.println("JSON file doesn't contain an array of objects");
+                    System.exit(-1);
+                }
+
+                if (!object.has("key")) {
+                    System.out.println("object is missing the field 'key'");
+                    System.exit(-1);
+                }
+
+                if (!object.get("key").isTextual()) {
+                    System.out.println("key is not a string");
+                    System.exit(-1);
+                }
+
+                if (!object.has("message")) {
+                    System.out.println("object is missing the field 'message'");
+                    System.exit(-1);
+                }
+
+                JsonNode message = object.get("message");
+
+                if (!message.isObject() && !message.isTextual()) {
+                    System.out.println("message is not a JSON object or a string");
+                    System.exit(-1);
+                }
+
+                String key = object.get("key").asText();
+                String payload;
+
+                if (message.isObject()) {
+                    payload = objectWriter.writeValueAsString(message);
+                } else if (message.isTextual()) {
+                    payload = message.asText();
+                } else {
+                    throw new RuntimeException("message is wrong type");
+                }
+
+                publisher.publish(topic, key, payload);
+
+                System.out.println("Wrote " + payload);
+            }
+        } finally {
+            publisher.close();
+        }
+    }
+
+
+    public void publishJSONMessages(String[] args, JSONArray JsonArray) throws IOException, ExecutionException, InterruptedException {
+        OptionParser parser = new OptionParser();
+
+        parser.acceptsAll(Lists.newArrayList("s", "servers"), "kafka servers").withRequiredArg().ofType(String.class);
+        parser.acceptsAll(Lists.newArrayList("t", "topic"), "kafka topic").withRequiredArg().ofType(String.class);
+        parser.acceptsAll(Lists.newArrayList("d", "delay"), "seconds delay between messages").withRequiredArg().ofType(Integer.class).defaultsTo(0);
+        parser.acceptsAll(Lists.newArrayList("h", "help"), "show help");
+
+        OptionSet options = parser.parse(args);
+
+        if (!options.hasArgument("servers")) {
+            parser.printHelpOn(System.out);
+            System.out.println("\nservers is required");
+            System.exit(-1);
+        }
+
+        if (!options.hasArgument("topic")) {
+            parser.printHelpOn(System.out);
+            System.out.println("\ntopic is required");
+            System.exit(-1);
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writer();
+
+        String topic = (String) options.valueOf("topic");
+
+        QioKafkaPublisher publisher = new QioKafkaPublisher((String) options.valueOf("servers"));
+        try {
+            Iterator<JsonNode> objects = JsonArray.iterator();
+            while (objects.hasNext()) {
+                JsonNode object = objects.next();
+                if (!object.isObject()) {
+                    System.out.println("JSON Array doesn't contain an array of objects");
                     System.exit(-1);
                 }
 
